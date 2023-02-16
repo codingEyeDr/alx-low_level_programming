506be8fb94d4ae4e695bc11a635f24d8c1a59cd1
codingEyeDr <anthonynzubechukwu5448@gmail.com>
2023-02-16 08:50:57 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - starting point for program execution
+ *
+ * Return: returns 0 when program is executed successfully
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
