4f43ae574383158fe6e1bed44e071fdc7d7a7497
codingEyeDr <anthonynzubechukwu5448@gmail.com>
2023-02-21 04:47:13 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - starting point for program execution
+ * _putchar: it prints the alphabets putchar
+ * Return: returns 0 when the program is executed successfully
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
