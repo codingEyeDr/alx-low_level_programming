4ffc5a439da121a465b4fb9e5ab820cb6e03c4c4
codingEyeDr <anthonynzubechukwu5448@gmail.com>
2023-04-09 14:12:57 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 
 void check_IO_stat(int stat, int fd, char *filename, char mode);
+
 /**
  * main - copies the content of one file to another
  * @argc: argument count
@@ -22,39 +23,26 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(1);
+		fprintf(stderr, "%s", "Usage: cp file_from file_to\n");
+		exit(97);
 	}
-
 	src = open(argv[1], O_RDONLY);
 	check_IO_stat(src, -1, argv[1], 'O');
-
 	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
 	check_IO_stat(dest, -1, argv[2], 'W');
-
 	while (n_read == 1024)
 	{
 		n_read = read(src, buffer, sizeof(buffer));
-
 		if (n_read == -1)
-		{
 			check_IO_stat(-1, -1, argv[1], 'O');
-		}
-
 		wrote = write(dest, buffer, n_read);
-
 		if (wrote == -1)
-		{
 			check_IO_stat(-1, -1, argv[2], 'W');
-		}
 	}
-
 	close_src = close(src);
 	check_IO_stat(close_src, src, NULL, 'C');
-
 	close_dest = close(dest);
 	check_IO_stat(close_dest, dest, NULL, 'C');
-
 	return (0);
 }
 
@@ -72,17 +60,17 @@ void check_IO_stat(int stat, int fd, char *filename, char mode)
 {
 	if (mode == 'C' && stat == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		fprintf(stderr, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 	else if (mode == 'O' && stat == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		fprintf(stderr, "Error: Can't read from file %s\n", filename);
 		exit(98);
 	}
 	else if (mode == 'W' && stat == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		fprintf(stderr, "Error: Can't write to %s\n", filename);
 		exit(99);
 	}
 }
